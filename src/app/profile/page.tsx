429e180efeeffe35424dbc9904333f40f5502a1d
LunaPedro17 <paluna@espol.edu.ec>
2025-06-15 00:46:20 -0500

    Creation of profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -5,7 +5,7 @@ import Navbar from '../components/navbar';
 export default function ProfilePage() {
   return (
     <main className="min-h-screen bg-gray-50 p-8">
-      <Navbar /> {/* Add the Navbar here */}
+      <Navbar /> 
       <UserProfileContent />
     </main>
   );
