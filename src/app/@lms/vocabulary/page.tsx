752880457d9dd7cdf2e0c574d038b711614356ef
Savier <133722978+savier018@users.noreply.github.com>
2025-08-14 22:47:49 -0500

    feat: Add vocabulary interface for user

---
+++ src/app/@lms/vocabulary/page.tsx
@@ -0,0 +1,9 @@
+import UserVocabularyContent from "@/components/modules/vocabulary/UserVocabularyContent";
+
+export default function VocabularyPage() {
+  return (
+    <div>
+      <UserVocabularyContent />
+    </div>
+  );
+}
