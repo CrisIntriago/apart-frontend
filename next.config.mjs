2512360037f231e09e60cbf79c010b73e1b6bfc5
CrisIntriago <caintria@espol.edu.ec>
2025-08-17 13:27:25 -0500

    feat: allow image sources

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-  domains: ['picsum.photos', 'localhost', 'lh3.googleusercontent.com'],
+    domains: ['picsum.photos', 'localhost', 'lh3.googleusercontent.com', 'apartbackend-production.up.railway.app', 'apart-backend-django.s3.us-east-1.amazonaws.com'],
   },
 };
 export default nextConfig;
